f870d21cc17a8524683c9fbbc80975a2f195c4d2
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2024-11-07 17:19:30 -0500

    Update UsuarioController.java

--- MsSecurity/src/main/java/TDB2024II/MsSecurity/controller/UsuarioController.java
+++ MsSecurity/src/main/java/TDB2024II/MsSecurity/controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package TDB2024II.MsSecurity.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +30,47 @@ public class UsuarioController {
         return lista;
     }
 
-    @GetMapping("/create")
+    @PostMapping("/create")
     public UsuarioModel create(@RequestBody UsuarioModel model){
         return usuarioService.add(model);
+    }
+
+    //findById
+
+    //update
+
+    //eliminar
+
+    @GetMapping("/findById/{id}")
+    public UsuarioModel findById(@PathVariable Integer id) {
+        return usuarioService.findById(id);
+    }
+
+    /* 
+    @PutMapping("/update/{id}")
+    public UsuarioModel update(@PathVariable Integer id, @RequestBody UsuarioModel model) {
+        model.set(id); // Asigna el ID al modelo para actualizar el usuario correspondiente
+        return usuarioService.update(model);
+    }*/
+    @PutMapping("/update/{id}")
+    public UsuarioModel update(@PathVariable Integer id, @RequestBody UsuarioModel newModelData) {
+        UsuarioModel existingUser = usuarioService.findById(id);
+        
+        if (id == existingUser.idUsuario) {
+            UsuarioModel userToUpdate = existingUser;
+
+            return usuarioService.update(userToUpdate); // Guarda y retorna el usuario actualizado
+        } else {
+            // Manejo de error, por ejemplo lanzando una excepción o retornando un error
+            throw new RuntimeException("Usuario no encontrado con id: " + id);
+        }
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        boolean isDeleted = usuarioService.delete(id);
+        return isDeleted ? "Usuario eliminado correctamente" : "Error al eliminar usuario";
     }
 
 
