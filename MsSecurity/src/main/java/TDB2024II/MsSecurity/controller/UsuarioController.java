d3b801eebbd1a8642a3db4e28a3e1bad4353a056
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2024-11-07 15:43:24 -0500

    Create UsuarioController.java

---
+++ MsSecurity/src/main/java/TDB2024II/MsSecurity/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package TDB2024II.MsSecurity.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import TDB2024II.MsSecurity.model.UsuarioModel;
+import TDB2024II.MsSecurity.services.UsuarioService;
+
+
+
+@RestController
+@RequestMapping("usuario")
+public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping("/findAll")
+    public List<UsuarioModel> findAll(){
+        List<UsuarioModel> lista = usuarioService.findAll();
+        return lista;
+    }
+    /*
+     try{
+        abrir conexion
+        insertar un registro
+     }
+     catch(Excepcion e){
+        se producjo una excepcion
+     }
+     finally{
+        cerrar conexion
+     }
+     */
+}
